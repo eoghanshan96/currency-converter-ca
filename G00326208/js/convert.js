121bc40c968fedc7df4741fc6dedc9bb5b01ea8b
eoghanshan96 <38285548+eoghanshan96@users.noreply.github.com>
2018-04-25 13:50:02 +0100

    javascript code fixed

--- G00326208/js/convert.js
+++ G00326208/js/convert.js
@@ -8,3 +8,34 @@ function euroToPounds()[
   doc.getElementById("resultsentence").innerHTML = message;
 
 ]
+
+ [function euroToDollars()
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var pounds = amount * 1.06;
+  var message = amount + ' euros converts to ' + dollars + ' dollars.';
+  console.log(message);
+  doc.getElementById("resultsentence").innerHTML = message; 
+  
+  ]
+  
+  [ function euroToYen()
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var pounds = amount * 121.12;
+  var message = amount + ' euros converts to ' + yen + ' yen';
+  console.log(message);
+  doc.getElementById("resultsentence").innerHTML = message; </script>
+  ]
+  
+  [ function euroToBitcoin()
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var pounds = amount * 0.00082;
+  var message = amount + ' euros converts to ' + bitcoin + ' bitcoin.';
+  console.log(message);
+  doc.getElementById("resultsentence").innerHTML = message; 
+  ]
